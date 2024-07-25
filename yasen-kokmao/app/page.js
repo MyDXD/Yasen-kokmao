ba2a2265cc7455aaa4c8f4b570ab202bd206261b
Hassadin Sasoh <dislove107@gmail.com>
2024-07-26 00:39:30 +0700

    update frontend homepage

--- yasen-kokmao/app/page.js
+++ yasen-kokmao/app/page.js
@@ -1,5 +1,52 @@
+import Image from "next/image";
+import { Kanit } from "@next/font/google";
+const kanit = Kanit({
+  weight: "400",
+  subsets: ["thai"],
+  display: "swap",
+});
+
 export default function Home() {
   return (
-    <h1>home page</h1>
+    <div className={`${kanit.className}`}>
+      <div className={kanit.className + " grid grid-cols-2 gap-2 bg-[#f2f3f4]"}>
+        <div className="flex mt-10 justify-center items-center">
+          <h3 className="text-center text-2xl  text-[#6c6d70]">
+            ยาเส้นโคกเมาของจริงของแท้<br></br>
+            ปลูกบ้านโคกเมา ขายโดนคนโคกเมา<br></br>
+            ยี่ห้อ
+            <span className="text-3xl font-bold	 text-[#81aa89]">
+              &nbsp; บังดีนยาเส้นโคกเมา &nbsp;
+            </span>
+            ชื่อนี้มั่นใจได้ครับ
+          </h3>
+        </div>
+        <div className="mt-10  h-96">
+          <Image
+            src="/images/all.jpg"
+            width={300}
+            height={500}
+            className="object-fill rounded-3xl  w-80 absolute   translate-y-0" // just an example
+          />
+        </div>
+      </div>
+      <div className="grid grid-col-2 m-60 mt-10 ">
+        <div className=" justify-start items-start">
+          <span className="text-3xl font-bold	 text-[#81aa89] underline underline-offset">
+            เกรดของยาเส้น
+          </span>
+          <p className="mt-5">
+            ทางร้านบังดีนยาเส้นโคกเมาของเรามีการคัดเกรดของยาเส้น…
+          </p>
+        </div>
+        <div className=" justify-start items-start mt-10">
+          <span className="text-3xl font-bold	 text-[#81aa89]">
+            บังดีนยาเส้นโคกเมา
+          </span>
+
+          <p>ทางร้านบังดีนยาเส้นโคกเมาของเรามีการคัดเกรดของยาเส้น…</p>
+        </div>
+      </div>
+    </div>
   );
 }
--- yasen-kokmao/components/Navbar.js
+++ yasen-kokmao/components/Navbar.js
@@ -1,28 +1,50 @@
 import "./Navbar";
 import Link from "next/link";
+import { Kanit } from "@next/font/google";
+import Image from "next/image";
+
+const kanit = Kanit({
+  weight: "400",
+  subsets: ["thai"],
+  display: "swap",
+});
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-center items-center p-4 border-2 border-gray-700">
-      <Link href="/" className="mx-4 hover:underline">
-        หน้าหลัก
-      </Link>
-      <Link href="/" className="mx-4 hover:underline">
-        สินค้าทั้งหมด
-      </Link>
-      <Link href="/" className="mx-4 hover:underline">
-        บทความ
-      </Link>
-      <Link href="/" className="mx-4 hover:underline">
-        ติดต่อเรา
-      </Link>
-      <Link href="/" className="mx-4 hover:underline">
-        เวลา
-      </Link>
-      <Link href="/" className="mx-4 hover:underline">
-        เบอร์โทร
-      </Link>
-    </nav>
+    <div className={`${kanit.className} `}>
+      <div className="flex flex-col justify-center  items-center m-10">
+        <Image
+          src="/images/logo.jpg"
+          width={150}
+          height={150}
+          className="content-center	rounded-full"
+        />
+        <p className="mt-5 text-lg text-gray-600">บังดีนยาเส้นโคกเมา</p>
+      </div>
+
+      <nav
+        className={`flex justify-center items-center p-7 border-2 border-gray-700 bg-[#ffffff]`}
+      >
+        <Link href="/" className="mx-4 hover:underline">
+          หน้าหลัก
+        </Link>
+        <Link href="/" className="mx-4 hover:underline">
+          สินค้าทั้งหมด
+        </Link>
+        <Link href="/" className="mx-4 hover:underline">
+          บทความ
+        </Link>
+        <Link href="/" className="mx-4 hover:underline">
+          ติดต่อเรา
+        </Link>
+        <Link href="/" className="mx-4 hover:underline">
+          เวลา
+        </Link>
+        <Link href="/" className="mx-4 hover:underline">
+          เบอร์โทร
+        </Link>
+      </nav>
+    </div>
   );
 };
 
